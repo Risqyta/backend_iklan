a3697b222baa62b5b9f5892b6fdbc6a3864e59b8
risqyta <dhivariskita17@gmail.com>
2020-03-24 21:49:54 +0700

    pertama

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+
+    return $request->user();
+    
+
+});
+Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@login');
+    Route::get('iklan', 'IklanController@iklan');
+    
+    Route::get('iklanall', 'IklanController@iklanAuth')->middleware('jwt.verify');
+    Route::get('user', 'UserController@getAuthenticatedUser')->middleware('jwt.verify'); 
